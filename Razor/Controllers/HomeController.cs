cc3f2d05b02490d5fbd34a9bbf4801124d40858e
Sasha1448 <agames1448@gmail.com>
2022-05-22 19:25:59 +0300

    [Added] MVC Controller

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Raxor.Models;
+
+namespace Raxor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product product = new Product
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Descriptin = "A boat for one person",
+                Categore = "Watersports",
+                Price = 275M
+            };
+            return View(product);
+        }
+    }
+}
